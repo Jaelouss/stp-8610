9aa4470c4b631047e98a60a26880ba7795b04d7c
Eugen Alekseev <devilsbrother1@gmail.com>
2025-04-12 14:27:51 +0300

    fufufu

--- src/js/header-modal.js
+++ src/js/header-modal.js
@@ -1,40 +1,29 @@
 const burger = document.querySelector('.burger-icon');
-const nav = document.querySelector('.box-navigation');
-const body = document.body;
+const nav = document.querySelector('#nav');
+const html = document.documentElement;
 const overlay = document.getElementById('overlay');
 
 burger.addEventListener('click', () => {
   burger.classList.toggle('open');
-    nav.classList.toggle('active');
-    body.classList.toggle('lock');
-    overlay.classList.toggle('active');
+  nav.classList.toggle('active');
+  html.classList.toggle('is-lock');
+  overlay.classList.toggle('active');
 });
 
 window.addEventListener('resize', () => {
-    const width = window.innerWidth;
+  const width = window.innerWidth;
 
-    if(width >= 1200)
-    {
-        nav.classList.remove('active');
-        burger.classList.remove('open');
-        body.classList.remove('lock');
-        overlay.classList.remove('active');
-    }
-  });
-
-  overlay.addEventListener('click', () => {
+  if (width >= 1200) {
     nav.classList.remove('active');
     burger.classList.remove('open');
-    body.classList.remove('lock');
+    html.classList.remove('is-lock');
     overlay.classList.remove('active');
-  });
+  }
+});
 
-const menuLinks = document.querySelectorAll('.list-navigation .link-navigation');
-menuLinks.forEach(link => {
-  link.addEventListener('click', () => {
-    nav.classList.remove('active'); 
-    burger.classList.remove('open'); 
-    body.classList.remove('lock'); 
-    overlay.classList.remove('active');
-  });
+overlay.addEventListener('click', e => {
+  nav.classList.remove('active');
+  burger.classList.remove('open');
+  html.classList.remove('is-lock');
+  overlay.classList.remove('active');
 });
